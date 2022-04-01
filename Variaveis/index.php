0aacca8a7a3abd91a627d4d9d1c85a5f70181b06
Raven <ana.github0@gmail.com>
2022-04-01 18:00:39 -0300

    Curso de PHP da plataforma CursoemVideo. Primeiro exemplo e aula sobre variáveis e seus tipos. Anotações sobre o PHP.

---
+++ Variaveis/index.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Variáveis e seus tipos</title>
+    <style>
+        body {
+            background-color: gray;
+            font-family: Arial, sans-serif;
+        }
+    </style>
+</head>
+<body>
+    <h1>Variáveis em PHP</h1>
+    <?php
+        // $n = 4;
+        // $no = "Gustavo";
+        // $n = 4.5;
+
+        // echo $no;
+        // echo "<br>";
+        // echo "<br>";
+        // echo $n;
+
+        $nome = "Gustavo";
+        $idade = 36;
+
+        echo $nome." Tem ".$idade." anos!";
+
+        echo "<br>";
+        echo "<br>";
+
+        $nome = "Maria";
+        $idade = 18;
+        echo  "$nome tem $idade anos!";
+
+        echo "<br>";
+        echo "<br>";
+
+        $nome = "João";
+        $idade = 20;
+        echo  '$nome tem $idade anos!';
+    ?>
+</body>
+</html>
