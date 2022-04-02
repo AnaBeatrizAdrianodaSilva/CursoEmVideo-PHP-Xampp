be657536e494920a2ac7dd4ef016372e9116514a
Raven <ana.github0@gmail.com>
2022-04-02 17:36:39 -0300

    Começo de aula sobre Operadores Relacionais

--- OperadoresRelacionais/comparacoes.php
+++ OperadoresRelacionais/comparacoes.php
@@ -4,16 +4,12 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Variáveis de Variáveis</title>
+    <title>Comparações em PHP</title>
     <link rel="stylesheet" href="../CSS/style.css">
 </head>
 <body>
     <?php
-        $x = "abc";
-        $$x = "def";
-        echo "O conteúdo da variável X é $x";
-        echo "<br>";
-        echo "A variável ABC criada recebeu o valor $abc"; // A variável criada recebeu o nome do valor atribuído a primeira variável.
+        
     ?>
 </body>
 </html>
